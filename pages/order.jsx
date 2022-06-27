08da059dc594bdd0bff5980558a3c302f534019d
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-06-27 16:47:42 +0430

    complete style and static logic

--- pages/order.jsx
+++ pages/order.jsx
@@ -2,70 +2,94 @@
 import React from "react";
 
 const Order = () => {
+  const status = 0;
+  const indexClass = (index) => {
+    if (index - status < 1) return "done ";
+    else if (index - status === 1) return "inProgress ";
+    return "undone ";
+  };
   return (
     <div className="p-[50px] flex">
-      <div className=" ">
-        <div className="flex-[2_2_0%]  ">
-          <table className="w-full border-separate [border-spacing:20px]  ">
-            <thead className="">
-              <tr>
-                <th>Order ID</th>
-                <th>Customer</th>
-                <th>Address</th>
-                <th>Total</th>
-              </tr>
-            </thead>
-            <tbody className="">
-              <tr className="text-center">
-                <td className="">
-                  <span className=" ">111111111111</span>
-                </td>
-                <td className="">
-                  <span>John Doe</span>
-                </td>
-                <td>
-                  <span>Elton st.212-33</span>
-                </td>
-                <td>
-                  <span>$102.50</span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div className="flex-1 bg-[#333] text-white rounded-lg   ">
-          <div className="w-[90%] max-h-[300px]  p-[50px] pt-[10px] flex flex-col justify-between">
-            <h2 className="mb-4 font-semibold text-2xl">CART TOTAL</h2>
-            <div>
-              <b className="mr-[10px]">Subtotal:</b>$102.50
+      <div className="flex-[2_2_0%]  ">
+        <div>
+          <div className="mb-6">
+            <table className="w-full border-separate [border-spacing:20px]  ">
+              <thead className="">
+                <tr>
+                  <th>Order ID</th>
+                  <th>Customer</th>
+                  <th>Address</th>
+                  <th>Total</th>
+                </tr>
+              </thead>
+              <tbody className="">
+                <tr className="text-center">
+                  <td className="">
+                    <span className=" ">111111111111</span>
+                  </td>
+                  <td className="">
+                    <span>John Doe</span>
+                  </td>
+                  <td>
+                    <span>Elton st.212-33</span>
+                  </td>
+                  <td>
+                    <span>$102.50</span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="flex w-full items-center justify-evenly">
+            <div className={`flex flex-col items-center  ${indexClass(0)}`}>
+              <img src="/img/paid.png" alt="" width={30} height={30} />
+              <span>Payment</span>
+              <div>
+                <img src="/img/checked.png" alt="" width={20} height={20} />
+              </div>
             </div>
-            <div>
-              <b className="mr-[10px]">Discount:</b>$0.00
+            <div className={`flex flex-col items-center ${indexClass(1)}`}>
+              <img src="/img/bake.png" alt="" width={30} height={30} />
+              <span>Preparing</span>
+              <div>
+                <img src="/img/checked.png" alt="" width={20} height={20} />
+              </div>
             </div>
-            <div>
-              <b className="mr-[10px]">Total:</b>$102.50
+            <div className={`flex flex-col items-center ${indexClass(2)}`}>
+              <img src="/img/bike.png" alt="" width={30} height={30} />
+              <span>On the Way</span>
+              <div>
+                <img src="/img/checked.png" alt="" width={20} height={20} />
+              </div>
             </div>
-            <button
-              className="h-[30px] font-bold cursor-pointer bg-white text-[#d1411e] mt-10 rounded-lg"
-              type={"button"}
-            >
-              PAID
-            </button>
+            <div className={`flex flex-col items-center ${indexClass(3)}`}>
+              <img src="/img/delivered.png" alt="" width={30} height={30} />
+              <span>Delivered</span>
+              <div>
+                <img src="/img/checked.png" alt="" width={20} height={20} />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <div>
-        <div className="w-[100px]">
-          <img src="/img/paid.png" alt="" />
-        </div>
-        <div className="w-[100px]">
-          <img src="/img/bake.png" alt="" />
-        </div>
-        <div className="w-[100px]">
-          <img src="/img/paid.png" alt="" />
-        </div>
-        <div className="w-[100px]">
-          <img src="/img/paid.png" alt="" />
+      <div className="flex-1 bg-[#333] text-white rounded-lg   ">
+        <div className="max-h-[300px]  p-[50px] pt-[10px] flex flex-col justify-between  ">
+          <h2 className="mb-4 font-semibold text-2xl">CART TOTAL</h2>
+          <div>
+            <b className="mr-[10px]">Subtotal:</b>$102.50
+          </div>
+          <div>
+            <b className="mr-[10px]">Discount:</b>$0.00
+          </div>
+          <div>
+            <b className="mr-[10px]">Total:</b>$102.50
+          </div>
+          <button
+            className="h-[30px] font-bold cursor-not-allowed bg-white text-[#d1411e] mt-10 rounded-lg"
+            type={"button"}
+          >
+            PAID
+          </button>
         </div>
       </div>
     </div>
@@ -73,3 +97,19 @@ const Order = () => {
 };
 
 export default Order;
+{
+  /* <div>
+  <div className="w-[100px]">
+    <img src="/img/paid.png" alt="" />
+  </div>
+  <div className="w-[100px]">
+    <img src="/img/bake.png" alt="" />
+  </div>
+  <div className="w-[100px]">
+    <img src="/img/paid.png" alt="" />
+  </div>
+  <div className="w-[100px]">
+    <img src="/img/paid.png" alt="" />
+  </div>
+</div>; */
+}
