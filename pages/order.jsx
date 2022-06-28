971f829662b3d8e5bf7a02b00f0411a99c1dd349
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-06-28 14:44:17 +0430

    responsive order page

--- pages/order.jsx
+++ pages/order.jsx
@@ -9,12 +9,12 @@ const Order = () => {
     return "undone ";
   };
   return (
-    <div className="p-[50px] flex">
+    <div className="p-[50px] flex flex-col sm:flex-row">
       <div className="flex-[2_2_0%]  ">
         <div>
           <div className="mb-6">
             <table className="w-full border-separate [border-spacing:20px]  ">
-              <thead className="">
+              <thead className="hidden sm:flex">
                 <tr>
                   <th>Order ID</th>
                   <th>Customer</th>
@@ -23,46 +23,70 @@ const Order = () => {
                 </tr>
               </thead>
               <tbody className="">
-                <tr className="text-center">
+                <tr className="text-center flex flex-col">
                   <td className="">
-                    <span className=" ">111111111111</span>
+                    <span className="text-lg before:content-['OrderID:'] before:font-medium ">
+                      111111111111
+                    </span>
                   </td>
                   <td className="">
-                    <span>John Doe</span>
+                    <span className="text-lg before:content-['Customer:'] before:font-medium ">
+                      John Doe
+                    </span>
                   </td>
                   <td>
-                    <span>Elton st.212-33</span>
+                    <span className="text-lg before:content-['Address:'] before:font-medium ">
+                      Elton st.212-33
+                    </span>
                   </td>
                   <td>
-                    <span>$102.50</span>
+                    <span className="text-lg before:content-['Total:'] before:font-medium ">
+                      $102.50
+                    </span>
                   </td>
                 </tr>
               </tbody>
             </table>
           </div>
-          <div className="flex w-full items-center justify-evenly">
-            <div className={`flex flex-col items-center  ${indexClass(0)}`}>
+          <div className="flex flex-col sm:flex-row  w-full items-center justify-evenly">
+            <div
+              className={`flex flex-col items-center  mb-5 sm:mb-0 ${indexClass(
+                0
+              )}`}
+            >
               <img src="/img/paid.png" alt="" width={30} height={30} />
               <span>Payment</span>
               <div>
                 <img src="/img/checked.png" alt="" width={20} height={20} />
               </div>
             </div>
-            <div className={`flex flex-col items-center ${indexClass(1)}`}>
+            <div
+              className={`flex flex-col items-center mb-5 sm:mb-0 ${indexClass(
+                1
+              )}`}
+            >
               <img src="/img/bake.png" alt="" width={30} height={30} />
               <span>Preparing</span>
               <div>
                 <img src="/img/checked.png" alt="" width={20} height={20} />
               </div>
             </div>
-            <div className={`flex flex-col items-center ${indexClass(2)}`}>
+            <div
+              className={`flex flex-col items-center mb-5 sm:mb-0 ${indexClass(
+                2
+              )}`}
+            >
               <img src="/img/bike.png" alt="" width={30} height={30} />
               <span>On the Way</span>
               <div>
                 <img src="/img/checked.png" alt="" width={20} height={20} />
               </div>
             </div>
-            <div className={`flex flex-col items-center ${indexClass(3)}`}>
+            <div
+              className={`flex flex-col items-center mb-5 sm:mb-0 ${indexClass(
+                3
+              )}`}
+            >
               <img src="/img/delivered.png" alt="" width={30} height={30} />
               <span>Delivered</span>
               <div>
@@ -72,8 +96,8 @@ const Order = () => {
           </div>
         </div>
       </div>
-      <div className="flex-1 bg-[#333] text-white rounded-lg   ">
-        <div className="max-h-[300px]  p-[50px] pt-[10px] flex flex-col justify-between  ">
+      <div className="flex-1 bg-[#333] text-white rounded-lg    ">
+        <div className="max-h-[300px] p-5 sm:p-[50px] pt-[10px] flex flex-col justify-between  ">
           <h2 className="mb-4 font-semibold text-2xl">CART TOTAL</h2>
           <div>
             <b className="mr-[10px]">Subtotal:</b>$102.50
