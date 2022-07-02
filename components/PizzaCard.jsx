1953501a0dae966ec5acdcbc75bce064a880144d
shaghayegh hosseini <shaghayegh.hoseini93@gmail.com>
2022-07-02 17:10:36 +0430

    get data in dynamic way

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,22 +1,28 @@
 /* eslint-disable @next/next/no-img-element */
 import React from "react";
+import Link from "next/link";
 
-const PizzaCard = () => {
+const PizzaCard = ({ pizza }) => {
   return (
     <div className="relative w-full md:w-[22%] flex flex-col items-center justify-center py-5 px-10 space-y-3 cursor-pointer hover:bg-slate-50">
-      <img
-        alt=""
-        src="/img/pizza.png"
-        // width="500px"
-        // height="500px"
-        className="w-[200px] h-[200px]  md:w-[250px] md:h-[200px]  lg:w-[350px] lg:h-[300px]"
-      />
+      <Link href={`/product/${pizza._id}`} passHref>
+        <img
+          alt=""
+          src={pizza.img}
+          // width="500px"
+          // height="500px"
+          className="w-[200px] h-[200px]  md:w-[250px] md:h-[250px] "
+        />
+      </Link>
+
       <h2 className="text-3xl sm:text-lg font-extrabold text-[#d1411e]">
-        Campagnolia
+        {pizza.title}
       </h2>
-      <span className="text-2xl sm:text-lg font-bold text-[#666]">$19.90</span>
+      <span className="text-2xl sm:text-lg font-bold text-[#666]">
+        ${pizza.prices[0]}
+      </span>
       <p className="text-2xl sm:text-lg text-center text-[#777]">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+        {pizza.desc}
       </p>
     </div>
   );
